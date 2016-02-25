48813abc4519ca6ab3ab07b8fe8986f9daa460c8
Kyle Frost <kylefrost@comcast.net>
2016-02-25 09:39:51 -0500

    Add sample config and database creator

--- mail.py
+++ mail.py
@@ -12,7 +12,7 @@ def send_diff_update(differences):
     msg['To'] = toaddr
     msg['Subject'] = "You have a wishlist price change."
      
-    body = "<h2>Hey Kyle.</h2><h3>Item(s) on your Amazon Wishlist had a price drop!</h3><ul>"
+    body = "<h2>Hey " + config.info.NAME + ".</h2><h3>Item(s) on your Amazon Wishlist had a price drop!</h3><ul>"
     
     for difference in differences:
         body += "<li><a href=\"http://amazon.com/dp/" + difference[1] + "\" target=\"_blank\">" + difference[0] + "</a>. Changed -$" + difference[2] + "</li>"
@@ -21,7 +21,7 @@ def send_diff_update(differences):
     
     msg.attach(MIMEText(body, 'html'))
      
-    server = smtplib.SMTP(config.email.SERVER, 587)
+    server = smtplib.SMTP(config.email.SERVER, config.email.PORT)
     server.starttls()
     server.login(fromaddr, config.email.PASSWD)
     text = msg.as_string()
---
+++ sample_config.py
@@ -0,0 +1,16 @@
+class database:
+    HOST="<HOST>"
+    USER="<USERNAME>"
+    PASSWD="<PASSWORD>"
+    DB="<DATABASE>"
+
+class info:
+    LISTID="<LISTID>"
+    NAME="<YOUR NAME>"
+
+class email:
+    FROM="<FROM EMAIL>"
+    TO="<TO EMAIL>"
+    PASSWD="<FROM EMAIL PASSWORD>"
+    SERVER="<SMTP SERVER>"
+    PORT=<SMTP PORT>
